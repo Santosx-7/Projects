a1420d7f01da76a28ffca01a17ccc1bca8164af2
Hydrax <danielreidosjogos@gmail.com>
2024-11-25 10:25:57 -0300

    Add files via upload

---
+++ naosei.py
@@ -0,0 +1,11 @@
+print('1, "Daniel"')
+print(12, 34, 56, sep="S", end="Lov")
+"""
+Print é usado para imprimir algo na tela
+"""
+# Eai
+print(type('Daniel'))
+print(type(1))
+print(type(0.1))
+# type diz o tipo do objeto
+print('Manuela')
